995e892a98fed63c2a5746073568223d5cebb8dc
Cat Hoang <cathoang@hotmail.co.uk>
2023-07-26 15:17:43 +0100

    remove unnecessary files

--- src/herd_the_babies/herd_the_babies.py
+++ src/herd_the_babies/herd_the_babies.py
@@ -1,13 +1,2 @@
-
-def herd_the_babies(stringWords):
-    finalStr = ''
-    sortString = sorted(stringWords, key=str.casefold)
-    newStr = ''.join(sortString)
-
-    for index, string in enumerate(newStr):
-        if index > 0:
-            if (newStr[index -1] > newStr[index]) == False:
-                print([index -1])
-                finalStr += newStr[index-1]
-      
-    print(finalStr)
+def herd_the_babies(string):
+    pass
