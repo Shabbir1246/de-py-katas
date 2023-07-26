ff6afe84f2833205e003145050bf14426c2e7af6
Cat Hoang <cathoang@hotmail.co.uk>
2023-07-26 11:50:39 +0100

    remove md files from src

--- src/herd_the_babies/herd_the_babies.py
+++ src/herd_the_babies/herd_the_babies.py
@@ -1,3 +1,13 @@
 
-def herd_the_babies(string):
-    pass
+def herd_the_babies(stringWords):
+    finalStr = ''
+    sortString = sorted(stringWords, key=str.casefold)
+    newStr = ''.join(sortString)
+
+    for index, string in enumerate(newStr):
+        if index > 0:
+            if (newStr[index -1] > newStr[index]) == False:
+                print([index -1])
+                finalStr += newStr[index-1]
+      
+    print(finalStr)
